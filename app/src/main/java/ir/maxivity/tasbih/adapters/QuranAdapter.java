ff52f6ef55d97f7e94b02ec874926f981edb23f5
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-06-27 15:23:12 +0430

    feat : add general adapter

--- app/src/main/java/ir/maxivity/tasbih/adapters/QuranAdapter.java
+++ app/src/main/java/ir/maxivity/tasbih/adapters/QuranAdapter.java
@@ -6,23 +6,45 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import ir.maxivity.tasbih.R;
 
-public class QuranAdapter extends RecyclerView.Adapter<QuranAdapter.QuranViewHolder> {
+public class QuranAdapter extends RecyclerView.Adapter<QuranAdapter.QuranViewHolder>
+        implements Filterable {
 
     private Context context;
     private JSONArray quran;
     private OnSurahClick listener;
+    private ArrayList<ListItemObject> items = new ArrayList<>();
+    private ArrayList<ListItemObject> itemsFilter = new ArrayList<>();
 
     public QuranAdapter(Context context, JSONArray quran) {
         this.context = context;
         this.quran = quran;
+
+        for (int i = 0; i < quran.length(); i++) {
+            ListItemObject obj = new ListItemObject();
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = quran.getJSONObject(i);
+                obj.name = jsonObject.getString("arabic");
+                obj.id = Integer.parseInt(jsonObject.getString("id"));
+                items.add(obj);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        itemsFilter = items;
     }
 
     @NonNull
@@ -33,20 +55,54 @@ public class QuranAdapter extends RecyclerView.Adapter<QuranAdapter.QuranViewHol
 
     @Override
     public void onBindViewHolder(@NonNull QuranViewHolder quranViewHolder, int i) {
-        try {
+      /*  try {
             quranViewHolder.setData(quran.getJSONObject(i));
         } catch (JSONException e) {
             e.printStackTrace();
-        }
+        }*/
+
+        quranViewHolder.setListData(itemsFilter.get(i));
     }
 
     @Override
     public int getItemCount() {
-        return quran.length();
+        return itemsFilter.size();
     }
 
     public void setOnSurahClickListener(OnSurahClick click) {
         this.listener = click;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String query = charSequence.toString();
+                if (query.isEmpty()) {
+                    itemsFilter = items;
+                } else {
+                    ArrayList<ListItemObject> filter = new ArrayList<>();
+                    for (ListItemObject object : items) {
+                        if (object.name.toLowerCase().contains(query.toLowerCase())) {
+                            filter.add(object);
+                        }
+                    }
+
+                    itemsFilter = filter;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = itemsFilter;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                itemsFilter = (ArrayList<ListItemObject>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
     public class QuranViewHolder extends RecyclerView.ViewHolder {
@@ -76,10 +132,26 @@ public class QuranAdapter extends RecyclerView.Adapter<QuranAdapter.QuranViewHol
                 e.printStackTrace();
             }
         }
+
+        public void setListData(final ListItemObject object) {
+            quranName.setText(object.name);
+            quranName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onClick(object.id);
+                }
+            });
+        }
     }
 
 
     public interface OnSurahClick {
         void onClick(int id);
+    }
+
+
+    class ListItemObject {
+        public String name;
+        public int id;
     }
 }
