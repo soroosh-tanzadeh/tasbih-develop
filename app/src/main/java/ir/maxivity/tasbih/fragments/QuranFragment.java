a2e27d8f34ea2528d1039c4add0d2ea2cc236370
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-06-27 12:56:05 +0430

    feat : add quran fragment

---
+++ app/src/main/java/ir/maxivity/tasbih/fragments/QuranFragment.java
@@ -0,0 +1,54 @@
+package ir.maxivity.tasbih.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class QuranFragment extends Fragment {
+
+    private JSONArray qurans;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            qurans = new JSONArray(readJsonFromAssets());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String readJsonFromAssets() {
+        String json = null;
+        try {
+            InputStream inputStream = getActivity().getAssets().open("surah_list.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+}
