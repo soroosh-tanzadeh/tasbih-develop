2ed52d6fe206213038ecf68cf4598dd2efe15e7a
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-06-28 18:43:04 +0430

    feat : add class

---
+++ app/src/main/java/tools/Utilities.java
@@ -0,0 +1,43 @@
+package tools;
+
+import android.support.annotation.NonNull;
+
+public class Utilities {
+
+
+    public static String numberConvert_Fa2En(@NonNull String rawStr) {
+        StringBuffer stringBuffer = new StringBuffer();
+        char c;
+        for (int i = 0; i < rawStr.length(); i++) {
+            c = rawStr.charAt(i);
+            if (c <= '۹' && c >= '۰')
+                stringBuffer.append((char) (c - '۰' + '0'));
+            else
+                stringBuffer.append(c);
+        }
+        return String.valueOf(stringBuffer);
+    }
+
+
+    // a mapping which used in numberConvert_En2Fa
+    private static String[] faDigits = new String[]{"۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"};
+
+
+    public static String numberConvert_En2Fa(@NonNull String rawStr) {
+        if (rawStr == null) {
+            return "";
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        char c;
+        for (int i = 0; i < rawStr.length(); i++) {
+            c = rawStr.charAt(i);
+            if (c <= '9' && c >= '0')
+                stringBuffer.append(faDigits[c - '0']);
+            else
+                stringBuffer.append(c);
+
+        }
+        return String.valueOf(stringBuffer);
+    }
+
+}
