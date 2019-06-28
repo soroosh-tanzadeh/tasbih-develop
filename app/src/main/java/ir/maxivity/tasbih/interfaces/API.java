20d309daecd8d379178391c489dc5025bb444dff
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-06-28 19:48:00 +0430

    ref : change body

--- app/src/main/java/ir/maxivity/tasbih/interfaces/API.java
+++ app/src/main/java/ir/maxivity/tasbih/interfaces/API.java
@@ -6,7 +6,6 @@ import ir.maxivity.tasbih.models.GetPlaces;
 import ir.maxivity.tasbih.models.GetQuranText;
 import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -17,6 +16,7 @@ public interface API {
     @POST("get-places.php")
     Call<ArrayList<GetPlaces.response>> getPlaces(@Part("data") RequestBody body);
 
+    @Multipart
     @POST("get-qurantext.php")
-    Call<ArrayList<GetQuranText>> getQuranText(@Body int sura);
+    Call<ArrayList<GetQuranText>> getQuranText(@Part("sura") RequestBody body);
 }
