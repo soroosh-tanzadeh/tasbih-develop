7cfeec15439b6fda331a3c9bc054a0351e32f4bf
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-06-29 22:44:14 +0430

    feat : add location body

---
+++ app/src/main/java/ir/maxivity/tasbih/models/AddNewLocationBody.java
@@ -0,0 +1,14 @@
+package ir.maxivity.tasbih.models;
+
+public class AddNewLocationBody {
+    public String user_id;
+    public String img_address;
+    public String img_documents;
+    public String place_name;
+    public String description;
+    public String phone;
+    public String web_address;
+    public String indate;
+    public String lat;
+    public String lon;
+}
