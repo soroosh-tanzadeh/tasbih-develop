40fb94f3b9f8a1669468b0a2e11e16b4db2e52ea
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-06-29 20:37:47 +0430

    feat : get input texts

--- app/src/main/java/ir/maxivity/tasbih/fragments/mapFragments/AddLocationInfoFragment.java
+++ app/src/main/java/ir/maxivity/tasbih/fragments/mapFragments/AddLocationInfoFragment.java
@@ -1,29 +1,53 @@
 package ir.maxivity.tasbih.fragments.mapFragments;
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import ir.maxivity.tasbih.R;
 import ir.maxivity.tasbih.adapters.CategorySpinnerAdapter;
 import ir.maxivity.tasbih.interfaces.MapListener;
 
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddLocationInfoFragment extends Fragment {
+
+    private static final int PICK_IMAGE = 200;
+    private static final String IMAGE_FILE_DESTINATION = "nasim_cropped_image";
+    private static final String TAG = "FUCK INFO";
+
 
     private MapListener listener;
 
@@ -31,6 +55,9 @@ public class AddLocationInfoFragment extends Fragment {
     private Button submit, cancel;
     private Spinner category;
     private EditText homeName, websiteAddress, phoneNumber;
+    private HashMap<String, String> infoFieldMap = new HashMap<>();
+    private ImageView addImage;
+    private String imageBase64Text;
     public AddLocationInfoFragment() {
         // Required empty public constructor
     }
@@ -52,6 +79,7 @@ public class AddLocationInfoFragment extends Fragment {
         homeName = root.findViewById(R.id.location_name_edt);
         websiteAddress = root.findViewById(R.id.website_edt);
         phoneNumber = root.findViewById(R.id.phone_edt_txt);
+        addImage = root.findViewById(R.id.add_image);
 
         CategorySpinnerAdapter adapter = new CategorySpinnerAdapter(getContext(), R.layout.category_spinner_layout, categoryList());
 
@@ -74,7 +102,8 @@ public class AddLocationInfoFragment extends Fragment {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                fillInputData();
+                listener.onAddLocationInfoSubmit(infoFieldMap);
             }
         });
 
@@ -85,8 +114,23 @@ public class AddLocationInfoFragment extends Fragment {
             }
         });
 
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImageFromGallery();
+            }
+        });
+
     }
 
+    private void fillInputData() {
+        infoFieldMap.put("img_address", imageBase64Text);
+        infoFieldMap.put("img_documents", category.getSelectedItem().toString());
+        infoFieldMap.put("place_name", homeName.getText().toString());
+        infoFieldMap.put("description", category.getSelectedItem().toString());
+        infoFieldMap.put("phone", phoneNumber.getText().toString());
+        infoFieldMap.put("web_address", websiteAddress.getText().toString());
+    }
 
     private List<String> categoryList() {
         List<String> list = new ArrayList<>();
@@ -102,6 +146,72 @@ public class AddLocationInfoFragment extends Fragment {
                 listener = (MapListener) getParentFragment();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void chooseImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+    private void startCrop(Uri uri) {
+        Date date = new Date();
+        String time = date.getTime() + "";
+        UCrop uCrop = UCrop.of(uri, Uri.fromFile(new File(getActivity().getCacheDir(), IMAGE_FILE_DESTINATION + "_" + time + ".png")));
+        uCrop.withAspectRatio(1, 1);
+        uCrop.withMaxResultSize(250, 250);
+
+        UCrop.Options options = new UCrop.Options();
+        options.setCompressionQuality(100);
+        options.setCompressionFormat(Bitmap.CompressFormat.PNG);
+
+        uCrop.withOptions(options);
+        uCrop.start(getActivity().getApplicationContext(), this);
+
+    }
+
+    private void handleCropResult(Uri uri) {
+        try {
+            InputStream inputStream = getActivity().getContentResolver().openInputStream(uri);
+            Bitmap selectedImage = BitmapFactory.decodeStream(inputStream);
+            Picasso.get().load(uri).into(addImage);
+            Log.v(TAG, "image" + uri);
+            imageBase64Text = imageToBase64(selectedImage);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String imageToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        String baseFormat = "data:image/png;base64,";
+        return baseFormat + Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case PICK_IMAGE:
+                    Uri selectedImage = data.getData();
+                    if (selectedImage != null) {
+                        startCrop(selectedImage);
+                    } else {
+                        Toast.makeText(getActivity(), getString(R.string.cannot_retrieve_image), Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+                case UCrop.REQUEST_CROP:
+                    handleCropResult(UCrop.getOutput(data));
+                    break;
+            }
         }
     }
 }
