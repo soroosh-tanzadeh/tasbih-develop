ffde6b15faf8a7ffae128cf8738a4a78240b4816
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-06-29 12:14:09 +0430

    feat : add class

---
+++ app/src/main/java/ir/maxivity/tasbih/models/LoginResponse.java
@@ -0,0 +1,9 @@
+package ir.maxivity.tasbih.models;
+
+public class LoginResponse {
+    public String user_id;
+    public String phoneNumber;
+    public String msg;
+    public String data;
+    public int resultcode;
+}
