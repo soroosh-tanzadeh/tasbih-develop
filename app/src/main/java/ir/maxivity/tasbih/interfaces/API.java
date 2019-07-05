80402e195f6c98611889b83ce97c20fc257a86ad
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-07-05 16:26:35 +0430

    feat : add route

--- app/src/main/java/ir/maxivity/tasbih/interfaces/API.java
+++ app/src/main/java/ir/maxivity/tasbih/interfaces/API.java
@@ -3,11 +3,13 @@ package ir.maxivity.tasbih.interfaces;
 import java.util.ArrayList;
 
 import ir.maxivity.tasbih.models.AddNewPlaceResponse;
+import ir.maxivity.tasbih.models.GetMessages;
 import ir.maxivity.tasbih.models.GetPlaces;
 import ir.maxivity.tasbih.models.GetQuranText;
 import ir.maxivity.tasbih.models.LoginResponse;
 import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -30,4 +32,7 @@ public interface API {
     @Multipart
     @POST("new-place.php")
     Call<AddNewPlaceResponse> addPlace(@Part("data") RequestBody body);
+
+    @GET("get-messages.php")
+    Call<GetMessages> getMessages();
 }
