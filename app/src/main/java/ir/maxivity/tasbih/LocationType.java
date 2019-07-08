47aea2c7cb1ee143146adcc0713ca5ae1eef83f2
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-07-08 13:18:36 +0430

    feat : add enum

---
+++ app/src/main/java/ir/maxivity/tasbih/LocationType.java
@@ -0,0 +1,13 @@
+package ir.maxivity.tasbih;
+
+public enum LocationType {
+    COFEESHOP,
+    HOSPITAL,
+    GYM,
+    DENTISTRY,
+    HOTEL,
+    MOSQUE,
+    UNIVERSITY,
+    SHOP,
+    OTHER
+}
