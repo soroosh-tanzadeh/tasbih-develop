f7b98e5562377c1c784194d9b4a89bfc9f49f299
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-07-09 14:03:14 +0430

    ref : modify class

--- app/src/main/java/ir/maxivity/tasbih/models/GetFavoritePlaces.java
+++ app/src/main/java/ir/maxivity/tasbih/models/GetFavoritePlaces.java
@@ -1,6 +1,17 @@
 package ir.maxivity.tasbih.models;
 
+import java.util.ArrayList;
+
 public class GetFavoritePlaces {
 
     public int result;
+    public ArrayList<FavoriteResponse> data;
+
+
+    public class FavoriteResponse {
+        public String id;
+        public String user_id;
+        public String place_id;
+    }
+
 }
