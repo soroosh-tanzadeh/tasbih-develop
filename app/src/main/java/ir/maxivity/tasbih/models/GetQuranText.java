cfd67944b6aaa9108d5cd077eed699b123cd8801
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-07-10 15:42:50 +0430

    ref : modify model

--- app/src/main/java/ir/maxivity/tasbih/models/GetQuranText.java
+++ app/src/main/java/ir/maxivity/tasbih/models/GetQuranText.java
@@ -1,11 +1,18 @@
 package ir.maxivity.tasbih.models;
 
+import java.util.ArrayList;
+
 public class GetQuranText {
 
-    public String index;
-    public String sura;
-    public String aya;
-    public String text;
+    public int result;
+    public ArrayList<QuranResponse> data;
 
+
+    public class QuranResponse {
+        public String index;
+        public String sura;
+        public String aya;
+        public String text;
+    }
 
 }
