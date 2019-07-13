99acd73e43d6cfa5f4849232fde813e5d6351e23
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-07-13 16:48:42 +0430

    feat : add dialog

--- app/src/main/java/ir/maxivity/tasbih/BaseActivity.java
+++ app/src/main/java/ir/maxivity/tasbih/BaseActivity.java
@@ -1,8 +1,11 @@
 package ir.maxivity.tasbih;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
@@ -32,5 +35,29 @@ public class BaseActivity extends AppCompatActivity {
 
     public void showShortToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public NasimDialog guestUserSignUpDialog(final Activity activity) {
+        final NasimDialog nasimDialog = new NasimDialog(this, NasimDialogTypes.QUESTION_TYPE);
+        nasimDialog.setDialogTitle("کاربر مهمان");
+        nasimDialog.setDialogcontent("برای دیدن این قسمت باید ثبت نام کنید.");
+        nasimDialog.setPositiveButton(getString(R.string.ok), new NasimDialog.OnPositiveButtonClick() {
+            @Override
+            public void onClick(View v) {
+                nasimDialog.dismiss();
+                finish();
+                Intent intent = new Intent(activity, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        nasimDialog.setNegativeButton(getString(R.string.no), new NasimDialog.OnNegativeButtonClick() {
+            @Override
+            public void onClick(View v) {
+                nasimDialog.dismiss();
+            }
+        });
+
+        return nasimDialog;
     }
 }
