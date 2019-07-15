8e5e3bb3a7ff0d854dbf9824602c916521dee865
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-07-15 17:02:49 +0430

    feat : add fragment to handle back press

---
+++ app/src/main/java/ir/maxivity/tasbih/fragments/mapFragments/BaseFragment.java
@@ -0,0 +1,10 @@
+package ir.maxivity.tasbih.fragments.mapFragments;
+
+import android.support.v4.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+    public boolean onBackPressed() {
+        return false;
+    }
+}
