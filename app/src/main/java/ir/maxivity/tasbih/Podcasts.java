46d353289e76f67353eb2aa38c79099b94d0b0ad
mohammad reza BGP <mmdrezabaqalpour@yahoo.com>
2019-07-20 11:03:44 +0430

    ref : some layouts

--- app/src/main/java/ir/maxivity/tasbih/Podcasts.java
+++ app/src/main/java/ir/maxivity/tasbih/Podcasts.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,11 +16,7 @@ public class Podcasts extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        LinearLayout l = new LinearLayout(getContext());
-        TextView t = new TextView(getContext());
-        t.setText("این بخش به زودی اضافه می‌شود");
-        t.setTextSize(30);
-        l.addView(t);
-        return l;
+        View root = inflater.inflate(R.layout.activity_ziarat_online, container, false);
+        return root;
     }
 }
